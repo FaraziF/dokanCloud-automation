9f42c3a9019a45f847771c93292fa332a839e9f1
FaraziF <farazi333@gmail.com>
2022-09-13 17:28:41 +0600

    Basic Configuration

--- tests/admin.spec.ts
+++ tests/admin.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from "@playwright/test";
+import config from '../config.json';
 
 test('Admin login validation', async ({ page }) => {
-    await page.goto('https://testing.dokandev.com/admin/login');
+    await page.goto('/admin/login');
     await expect(page.locator("//span[text()='Sign In']")).toBeVisible();
-    await page.locator('input[name="email"]').fill('');
-    await page.locator('input[name="password"]').fill('');
+    await page.locator('input[name="email"]').fill(config.username_field);
+    await page.locator('input[name="password"]').fill(config.password_field);
     await page.locator("input[type='checkbox']").check();
     await page.locator("//span[text()='Sign In']").click();
     const dashboard = await page.innerText("//h1[text()='Dashboard']");

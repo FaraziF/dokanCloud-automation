8d9eee5438b72e14572d534967f8f82c4554ce61
FaraziF <farazi333@gmail.com>
2022-10-19 19:26:30 +0600

    update yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 // Read from default ".env" file.
 dotenv.config();
 
-const env = require('./env')
+const env = require('./env');
 
 /**
  * Read environment variables from file.
@@ -45,8 +45,8 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: env('URL'),
-    baseURL: 'https://testing.dokandev.com/',
+    baseURL: env('URL'),
+    // baseURL: 'https://testing.dokandev.com/',
     // baseURL: process
     // storageState: 'storageState.json',
    

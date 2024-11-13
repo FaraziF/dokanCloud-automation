abf88140366f238d1c341bbb5abf0cdd1e571e46
FaraziF <farazi333@gmail.com>
2024-11-13 11:50:55 +0600

    add api test coverage and modified bulk edit

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,14 +1,19 @@
-import { Browser, BrowserContextOptions, Page } from "@playwright/test";
+import { Browser, BrowserContextOptions, Page } from '@playwright/test';
 
 const { LOCAL, SITE_PATH } = process.env;
 
 export const helpers = {
   // create env
-    createEnvVar(key: string, value: string) {
-      console.log(`${key}=${value}`);
-      const content = '\n' + `${key}=${value}`;
-      process.env[key] = value;
-      // this.appendFile('.env', content); // for local testing
-    },
+  createEnvVar(key: string, value: string) {
+    console.log(`${key}=${value}`);
+    const content = '\n' + `${key}=${value}`;
+    process.env[key] = value;
+    // this.appendFile('.env', content); // for local testing
+  },
 
-}
+  // escape regex
+  escapeRegex: (str: string): string => {
+    const escapePatten = /[.*+\-?^$|(){}[\]\\]/g; // Special Regex Characters: ., *, +,-, ?, ^, $, |, (, ), {, }, [, ], \, ],
+    return str.replace(escapePatten, '\\$&'); // $& means the whole matched string
+  },
+};

ba7c656a1600830c734fa45b715d099b6ce02f2e
FaraziF <farazi333@gmail.com>
2025-07-31 14:53:33 +0600

    update time

--- tests/e2e/pages/loginPage.ts
+++ tests/e2e/pages/loginPage.ts
@@ -54,7 +54,7 @@ export class LoginPage {
     return await isVisible(this.page, selector.login.loginPasswordErrorMessage);
   }
 
-  // user manual Login Login with
+  // user manual Login Login with for admin
   async adminManualLogin(
     user: user,
     url: string = endPoints.adminDashboardLogin,
